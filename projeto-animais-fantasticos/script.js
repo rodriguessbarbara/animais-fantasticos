72daa407f71bb38b60716b29f7a1bd9e1bb5c8c4
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2022-05-19 14:49:42 -0300

    alteração na pasta script.js

--- projeto-animais-fantasticos/script.js
+++ projeto-animais-fantasticos/script.js
@@ -1,17 +1,6 @@
-const imgs = document.querySelectorAll('img');
+const img = document.querySelector('img');
+function callback() {
+    console.log('clicou');
+}
 
-imgs.forEach((imagem, index) => { //1o. item é o elementoHTML ; 2o. elemento é o index(i) ; o 3o. elemento é o próprio array 
-    console.log(imagem, index);
-});
-
-// ou pode ficar:
-//imgs.forEach((imagem) => console.log(imagem));
-
-
-//mostre no console cada paragrafo do site
-const paragrafos = document.querySelectorAll('p');
-paragrafos.forEach((paragrafo) => console.log(paragrafo));
-
-
-//mostre o texto dos parágrafos no console
-paragrafos.forEach((paragrafo) => console.log(paragrafo.innerText));
+img.addEventListener('click', callback);
