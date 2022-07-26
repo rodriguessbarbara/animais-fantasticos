6dcccdd3c45293cd6ad9e315b83fd035680a1dfe
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2022-07-26 14:26:55 -0300

    refactor: :art: melhorado codigo
    para navegação e organizacao

--- projeto-animais-fantasticos/script.js
+++ projeto-animais-fantasticos/script.js
@@ -1,16 +1,23 @@
-const tabmenu = document.querySelectorAll('.js-tabmenu li');
-const tabcontent = document.querySelectorAll('.js-tabcontent section');
-console.log(tabmenu);
+function tabNav() {
+    const tabmenu = document.querySelectorAll('.js-tabmenu li');
+    const tabcontent = document.querySelectorAll('.js-tabcontent section');
 
-function activeTab(index) {
-    tabcontent.forEach((section) => {
-        section.classList.remove('ativo');
-    });
-    tabcontent[index].classList.add('ativo');
+    if (tabmenu.length && tabcontent.length) {
+        tabcontent[0].classList.add('ativo');
+
+        function activeTab(index) {
+            tabcontent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabcontent[index].classList.add('ativo');
+        }
+
+        tabmenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            })
+        });
+    }
 }
 
-tabmenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    })
-});
+tabNav();
