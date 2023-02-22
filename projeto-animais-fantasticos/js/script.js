667fbfc1f6c1ecd57ce539e959393029f1c4f774
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-02-22 17:19:01 -0300

    refactor: :recycle: scrollSuave transformado em classe

--- projeto-animais-fantasticos/js/script.js
+++ projeto-animais-fantasticos/js/script.js
@@ -1,6 +1,6 @@
 import chamaTabNav from './modules/tabNav.js';
 import chamaAccordion from './modules/accordion.js';
-import chamaScrollSuave from './modules/animacaoScroll.js';
+import chamaScrollSuave from './modules/scrollSuave.js';
 import chamaAnimacaoScroll from './modules/animacaoScroll.js';
 import chamaModal from './modules/modal.js';
 import chamaTooltip from './modules/tooltip.js';
@@ -8,9 +8,11 @@ import chamaDropDown from './modules/menuDropDown.js';
 import chamaMenuMobile from './modules/menuMobile.js';
 import chamaAnimacaoNumeros from './modules/animacaoNumeros.js';
 
+const scrollSuave = new chamaScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
 chamaTabNav();
 chamaAccordion();
-chamaScrollSuave();
 chamaAnimacaoScroll();
 chamaModal();
 chamaTooltip();
